94ac7f8e31ceb07bd5d61ab42a971e928715e50c
Kacper <tarchala.k@gmail.com>
2014-07-04 18:50:24 +0200

    ending static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,11 +6,29 @@ describe "Static_Pages " do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
   describe "Help page" do
   	it "Should have content 'Help'" do
   		visit '/static_pages/help'
   		expect(page).to have_content('Help')
   	end
+    it "should have right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+  describe "About us" do 
+  	it "should have content 'About Us'" do
+  		visit '/static_pages/about_us'
+  		expect(page).to have_content('About us')
+  	end
+    it "should have right title" do
+      visit '/static_pages/about_us'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About us")
+    end
   end
 end

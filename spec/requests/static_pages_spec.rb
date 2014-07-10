c0be31134fdc4e512811b8f91ffa3ffce85b2fc3
Kacper <tarchala.k@gmail.com>
2014-07-10 16:20:39 +0200

    Complete bootstrap

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,34 +1,32 @@
 require 'spec_helper'
 
 describe "Static_Pages " do
+
+  let(:rortsa) {"Ruby on Rails Tutorial Sample App | "}
+  let(:rorts) {"Ruby on Rails Tutorial Sample App"}
+  subject { page }
+
   describe "Home Page" do
-    it "should have content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-    it "should have right title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end
+    before {visit root_path}
+    it { should have_content('Sample App') }
+    it { should have_title("#{rorts}") }
+    it { should_not have_title('| Home') }
   end
+
   describe "Help page" do
-  	it "Should have content 'Help'" do
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Help')
-  	end
-    it "should have right title" do
-      visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+    before { visit help_path }
+  	it { should have_content('Help') }
+    it { should have_title("#{rortsa} Help") }
   end
-  describe "About us" do 
-  	it "should have content 'About Us'" do
-  		visit '/static_pages/about_us'
-  		expect(page).to have_content('About us')
-  	end
-    it "should have right title" do
-      visit '/static_pages/about_us'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About us")
-    end
+
+  describe "About us" do
+    before { visit about_us_path } 
+  	it { should have_content('About Us') }  	
+    it { should have_title("#{rortsa} About Us") }
+  end
+  describe "Contact" do
+    before { visit contact_path }
+    it { should have_title("#{rortsa} Contact") }
+    it { should have_content("Contact") }
   end
 end

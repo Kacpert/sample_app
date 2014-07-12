f5ddeaa9d31c9ad3fbe8fb8fc173bc3ea7ee5e82
Kacper <tarchala.k@gmail.com>
2014-07-12 14:42:22 +0200

    Last save on 12.04

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   validates(:name, presence: true, length: { maximum: 50 })
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates(:email, presence: true, uniqueness: { case_sensitive: false },
+  			 format: { with: VALID_EMAIL_REGEX })
+  before_save { self.email = email.downcase }
 end

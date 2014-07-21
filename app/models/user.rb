00dc815c2a72f8ff02ca0cde6fbfb32161b0bfac
Kacper <tarchala.k@gmail.com>
2014-07-21 13:03:39 +0200

    password and valid user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   validates(:email, presence: true, uniqueness: { case_sensitive: false },
   			 format: { with: VALID_EMAIL_REGEX })
   before_save { self.email = email.downcase }
+  has_secure_password
+  validates(:password, length: { minimum: 6 })
 end

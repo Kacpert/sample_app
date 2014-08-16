94a9bbf80ee13caf0d00bb96592c7de67b1a2a43
Kacper <tarchala.k@gmail.com>
2014-08-16 14:33:41 +0200

    last

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,33 @@
 class UsersController < ApplicationController
 
+  before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Użytkownik usunięty."
+    redirect_to users_url
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+    def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profil zaktywowany"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def show
   	@user = User.find(params[:id])
   end
@@ -12,10 +40,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Witaj na blogu"
       redirect_to @user
     else
-      flash[:fail] = "try again"
+      flash[:fail] = "spróbuj jeszcze raz"
       render 'new'
     end
   end
@@ -25,6 +53,23 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Proszę się zalogować"
+      end
+
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 

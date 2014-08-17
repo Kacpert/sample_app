86ceb6ca141041df8f2ba797e47228f0f7a1d01a
Kacper <tarchala.k@gmail.com>
2014-08-17 22:48:50 +0200

    before 11

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -28,6 +28,14 @@ module SessionsHelper
     user == current_user
   end
 
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Proszę się zalogować"
+      end
+    end
+
     def signed_in?
     !current_user.nil?
   end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,11 +13,11 @@ describe "UserPages" do
     describe "page" do
       it { should have_content("Update your profile") }
       it { should have_title("Edit user") }
-      it { should have_link('change', href: 'http://gravatar.com/emails') }
+      it { should have_link('Zmień', href: 'http://gravatar.com/emails') }
     end
 
     describe "with invalid information" do
-      before { click_button "Save changes" }
+      before { click_button "Zapisz zmiany" }
 
       it { should have_content('error') }
     end
@@ -59,7 +59,7 @@ describe "UserPages" do
 
         it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_selector('div.alert.alert-success', text: 'Witaj') }
       end
     end
   end
